72c35630795599f82675189b00bea05ae36feff6
Blancamauri <Blamaurob@alum.us.es>
2021-04-24 18:15:47 +0200

    Task 23

--- src/main/java/acme/features/manager/workPlan/ManagerWorkPlanController.java
+++ src/main/java/acme/features/manager/workPlan/ManagerWorkPlanController.java
@@ -1,11 +1,13 @@
 package acme.features.manager.workPlan;
 
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import acme.components.CustomCommand;
 import acme.entities.roles.Manager;
 import acme.entities.workPlans.WorkPlan;
 import acme.framework.components.BasicCommand;
@@ -20,10 +22,35 @@ public class ManagerWorkPlanController extends AbstractController<Manager, WorkP
 	@Autowired
 	private ManagerWorkPlanListMineService listService;
 
+	@Autowired
+	private ManagerWorkPlanCreateService	createService;
+	
+	@Autowired
+	private ManagerWorkPlanDeleteService	deleteService;
+	
+	@Autowired
+	private ManagerWorkPlanPublishService	publishService;
+	
+	@Autowired
+	private ManagerWorkPlanShowService	showService;
+
+	@Autowired
+	private ManagerWorkPlanUpdateService	updateService;
+
+	
+
+	
+
 	// Constructors -----------------------------------------------------------
 
 	@PostConstruct
 	private void initialise() {
 		super.addBasicCommand(BasicCommand.LIST, this.listService);
+		super.addBasicCommand(BasicCommand.CREATE, this.createService);
+		super.addBasicCommand(BasicCommand.UPDATE, this.updateService);
+		super.addBasicCommand(BasicCommand.SHOW, this.showService);
+		super.addBasicCommand(BasicCommand.DELETE, this.deleteService);
+		super.addCustomCommand(CustomCommand.PUBLISH, BasicCommand.UPDATE, this.publishService);
 	}
+
 }
