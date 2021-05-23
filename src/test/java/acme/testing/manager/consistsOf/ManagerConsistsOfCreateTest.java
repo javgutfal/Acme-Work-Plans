5bc844f8999856085be831dcd4f62400f605217a
DiegoHill <diegillos@alum.us.es>
2021-05-23 18:41:59 +0200

    TASK 050: TEST MANAGER - CONSISTS OF
    Created 1 extra positive case for creation.

--- src/test/java/acme/testing/manager/consistsOf/ManagerConsistsOfCreateTest.java
+++ src/test/java/acme/testing/manager/consistsOf/ManagerConsistsOfCreateTest.java
@@ -22,9 +22,9 @@ public class ManagerConsistsOfCreateTest extends AcmePlannerTest{
 	}
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/manager/consists-of/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/manager/consists-of/create-positive-public.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void createPositive(final int recordIndex1,final int recordIndex2, final String title, final String initialTime, final String finalTime, final String workload, final String link, final String description, final String publicTask, final String publicTaskShow,final int recordIndex3) {
+	public void createPositivePublicWorkplan(final int recordIndex1,final int recordIndex2, final String title, final String initialTime, final String finalTime, final String workload, final String link, final String description, final String publicTask, final String publicTaskShow,final int recordIndex3) {
 		super.signIn("manager", "asdf1234");
 		super.clickOnMenu("Manager", "View workplans");
 		
@@ -62,7 +62,47 @@ public class ManagerConsistsOfCreateTest extends AcmePlannerTest{
 	}
 	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/manager/consists-of/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/manager/consists-of/create-positive-private.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void createPositivePrivateWorkplan(final int recordIndex1,final int recordIndex2, final String title, final String initialTime, final String finalTime, final String workload, final String link, final String description, final String publicTask, final String publicTaskShow,final int recordIndex3) {
+		super.signIn("manager", "asdf1234");
+		super.clickOnMenu("Manager", "View workplans");
+		
+		super.clickOnListingRecord(recordIndex1);
+		
+		super.clickOnButton("Add tasks");
+		
+		super.clickOnListingRecord(recordIndex2);
+		
+		super.clickOnButton("Add Task");
+		
+		super.clickOnMenu("Manager", "View workplans");
+		
+		super.clickOnListingRecord(recordIndex1);
+		
+		super.clickOnButton("List of tasks");
+		
+		super.checkColumnHasValue(recordIndex3, 0, title);
+		super.checkColumnHasValue(recordIndex3, 1, initialTime);
+		super.checkColumnHasValue(recordIndex3, 2, finalTime);
+		super.checkColumnHasValue(recordIndex3, 3, workload);
+		super.checkColumnHasValue(recordIndex3, 4, link);
+		super.checkColumnHasValue(recordIndex3, 5, description);
+		super.checkColumnHasValue(recordIndex3, 6, publicTask);
+		
+		super.clickOnListingRecord(recordIndex3);
+		
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("initialTime", initialTime);
+		super.checkInputBoxHasValue("finalTime", finalTime);
+		super.checkInputBoxHasValue("workload", workload);
+		super.checkInputBoxHasValue("link", link);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("publicTask", publicTaskShow);
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/consists-of/create-positive-public.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void createPositiveAccessWithURL(final int recordIndex1,final int recordIndex2, final String title, final String initialTime, final String finalTime, final String workload, final String link, final String description, final String publicTask, final String publicTaskShow,final int recordIndex3) {
 		super.signIn("manager", "asdf1234");
