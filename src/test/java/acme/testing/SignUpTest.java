0f31dd831b822ce4578a03bad6c9002bee9d727f
DiegoHill <diegillos@alum.us.es>
2021-05-24 15:05:19 +0200

    TASK 64: TEST SIGN UP
    Some tests implemented.

--- src/test/java/acme/testing/SignUpTest.java
+++ src/test/java/acme/testing/SignUpTest.java
@@ -12,9 +12,42 @@
 
 package acme.testing;
 
-public class SignUpTest {
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
 
-	// This is a placeholder where you can introduce your own sign-up test
-  	// Note that it depends on your project-specific test class.
+public class SignUpTest extends AcmePlannerTest{
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/sign-up/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveSignUp(final String username, final String password, final String name, final String surname, final String email) {
+		this.signUp(username, password, name, surname, email);
+		this.signIn(username, password);
+		assert super.exists(By.linkText("Account"));
+		this.signOut();
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/sign-up/negative-username-already-taken.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void negativeSignUpUsernameTaken(final String username, final String password, final String name, final String surname, final String email, final String phone) {
+		this.signUpFail(username, password, name, surname, email);
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/sign-up/negative-email-already-taken.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void negativeSignUpEmailTaken(final String username, final String password, final String name, final String surname, final String email, final String phone) {
+		this.signUpFail(username, password, name, surname, email);
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/sign-up/negative-password-short.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void negativeSignUpPasswordShort(final String username, final String password, final String name, final String surname, final String email, final String phone) {
+		this.signUpFail(username, password, name, surname, email);
+	}
 	
 }
